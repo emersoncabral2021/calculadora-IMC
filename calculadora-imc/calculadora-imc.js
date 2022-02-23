97edebaaa09831955fb25ec9f93cea2120178125
emerson da conceição cabral <emersoncabral202@gmail.com>
2022-02-23 20:44:54 -0300

    css

--- calculadora-imc/calculadora-imc.js
+++ calculadora-imc/calculadora-imc.js
@@ -1,19 +1,19 @@
 function calcular(){
 var peso = document.getElementById(`peso`).value
 var altura = document.getElementById(`altura`).value   
-var calcular = (peso /( altura * altura))
+var calcular = (peso /( altura * altura)).toFixed(2)
 var resultado = document.getElementById(`resultado`)
-
 if(calcular < 18){ 
-    resultado.innerText =  calcular + ` vc está abaixo do peso`
+    console.log(calcular)
+    resultado.innerText =  calcular + ` você está abaixo do peso`
 }else if(calcular > 18 && calcular < 24){
-    resultado.innerText = calcular + ` vc está no peso normal`
+    resultado.innerText = calcular + ` você está no peso normal`
     
 }else if(calcular > 24 && calcular < 30){
-    resultado.innerText = calcular + ` vc está com sobrepeso`
+    resultado.innerText =  + calcular + ` você está com sobrepeso`
     
 }else if( calcular > 30){
-    resultado.innerText = calcular + ` vc está acima do peso`
+    resultado.innerText = calcular + ` você está acima do peso`
     
 }
 
