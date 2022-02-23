ed945119d2fbc258bfaea5a38522aaddf8615538
emerson da conceição cabral <emersoncabral202@gmail.com>
2022-02-22 22:45:59 -0300

    iniciando css

--- calculadora-imc/calculadora-imc.js
+++ calculadora-imc/calculadora-imc.js
@@ -4,5 +4,17 @@ var altura = document.getElementById(`altura`).value
 var calcular = (peso /( altura * altura))
 var resultado = document.getElementById(`resultado`)
 
-resultado.innerText = calcular
+if(calcular < 18){ 
+    resultado.innerText =  calcular + ` vc está abaixo do peso`
+}else if(calcular > 18 && calcular < 24){
+    resultado.innerText = calcular + ` vc está no peso normal`
+    
+}else if(calcular > 24 && calcular < 30){
+    resultado.innerText = calcular + ` vc está com sobrepeso`
+    
+}else if( calcular > 30){
+    resultado.innerText = calcular + ` vc está acima do peso`
+    
+}
+
 }
